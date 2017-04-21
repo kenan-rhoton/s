f6c7fff767cecc46a768a982d62575b289f4872e
Kenan Rhoton <krhoton@redsauce.net>
2017-04-21 18:36:47 +0200

    Bolt DB

---
+++ db.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"os"
+	"time"
+)
+
+type DB struct {
+	name string
+	bolt *bolt.DB
+}
+
+func UseDatabase(name string) (*DB, error) {
+	db := &DB{}
+	db.name = name
+	err := db.Create()
+	return db, err
+}
+
+func (db *DB) Create() error {
+	var err error
+	db.bolt, err = bolt.Open(db.name+".db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		return err
+	}
+	err = db.bolt.Update(func(tx *bolt.Tx) error {
+		_, e := tx.CreateBucketIfNotExists([]byte("ID"))
+		if e != nil {
+			return fmt.Errorf("create bucket: %s", e)
+		}
+		_, e = tx.CreateBucketIfNotExists([]byte("Messages"))
+		if e != nil {
+			return fmt.Errorf("create bucket: %s", e)
+		}
+		return nil
+	})
+
+	return err
+}
+
+func (db *DB) Close() error {
+	return db.bolt.Close()
+}
+
+func (db *DB) Reset() error {
+	err := db.Close()
+	if err != nil {
+		return err
+	}
+	err = os.Remove(db.name + ".db")
+	if err != nil {
+		return err
+	}
+	db.Create()
+	return err
+
+}
+
+func (db *DB) SaveID(user string, key []byte) error {
+	return db.bolt.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("ID"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		err = b.Put([]byte(user), key)
+		return err
+	})
+}
+
+func (db *DB) GetKey(user string) ([]byte, error) {
+	var res []byte
+	err := db.bolt.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("ID"))
+		if b == nil {
+			return fmt.Errorf("bucket does not exist")
+		}
+		key := b.Get([]byte(user))
+		if key == nil {
+			return fmt.Errorf("invalid target")
+		}
+		res = make([]byte, len(key), (cap(key)+1)*2)
+		copy(res, key)
+		return nil
+	})
+	return res, err
+}
+
+func (db *DB) SaveMessage(user string, msg []byte) error {
+	_, err := db.GetKey(user)
+	if err != nil {
+		return err
+	}
+	return db.bolt.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("Messages"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		u, err := b.CreateBucketIfNotExists([]byte(user))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		num, _ := u.NextSequence()
+		err = u.Put(itob(num), msg)
+		return err
+	})
+}
+
+// itob returns an 8-byte big endian representation of v.
+func itob(v uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, v)
+	return b
+}
+
+func (db *DB) GetMessages(user string) ([][]byte, error) {
+	res := make([][]byte, 0)
+	err := db.bolt.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("Messages"))
+		if b == nil {
+			return fmt.Errorf("bucket does not exist")
+		}
+		u := b.Bucket([]byte(user))
+		if u == nil {
+			return fmt.Errorf("bucket does not exist")
+		}
+		c := u.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			temp := make([]byte, len(v), (cap(v)+1)*2)
+			copy(temp, v)
+			res = append(res, temp)
+		}
+		return nil
+	})
+	return res, err
+}
---
+++ db_test.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"testing"
+)
+
+type testID struct {
+	Alias string
+	PKey  []byte
+}
+
+type testMessage struct {
+	target  string
+	message []byte
+}
+
+func TestSaveIdentity(t *testing.T) {
+	db, err := UseDatabase("test")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer db.Close()
+	err = db.Reset()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	testdata := []testID{
+		{"boop", []byte("n12ui3h102h312undiqna0cpc+ÀW+")},
+		{"bep", []byte("FA SFOA MFMWE QTM QRQ  ZX;c.-xc,af e q")},
+	}
+	for _, v := range testdata {
+		err = db.SaveID(v.Alias, v.PKey)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		res, err := db.GetKey(v.Alias)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		if !same(res, v.PKey) {
+			t.Errorf("Wrong key: expected \"%s\" but got \"%s\"\n", v.PKey, res)
+		}
+	}
+}
+
+func same(one, two []byte) bool {
+	if len(one) != len(two) {
+		return false
+	}
+	for i := 0; i < len(one); i++ {
+		if one[i] != two[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestSaveMessage(t *testing.T) {
+	db, err := UseDatabase("test")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer db.Close()
+	err = db.Reset()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	testdata := []testMessage{
+		{"testuser", []byte("lol")},
+		{"testuser", []byte("rofl")},
+		{"testuser", []byte("duude")},
+		{"testuser", []byte("monkeey")},
+	}
+	err = db.SaveID("testuser", []byte("whatever, really"))
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	messagelist := make([][]byte, 0)
+	for _, v := range testdata {
+		err = db.SaveMessage(v.target, v.message)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+		messagelist = append(messagelist, v.message)
+	}
+	msgs, err := db.GetMessages("testuser")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	for _, m := range messagelist {
+		found := false
+		for _, msg := range msgs {
+			if same(m, msg) {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("Did not receive message %s", m)
+		}
+	}
+}
+
+func TestSaveMessageWithInvalidTarget(t *testing.T) {
+	db, err := UseDatabase("test")
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer db.Close()
+	err = db.Reset()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	expected := "invalid target"
+	err = db.SaveMessage("testuser", []byte("doesn't matter"))
+	if err == nil {
+		t.Errorf("Did not get an error with nonexistent user!")
+	} else {
+		if err.Error() != expected {
+			t.Errorf("Got wrong error, expected %s, got %s", expected, err.Error())
+		}
+	}
+}
